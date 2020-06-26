325f82d2c2ed90fc032b322454d90eb4cf0374af
bikashpattnaik37 <67461905+bikashpattnaik37@users.noreply.github.com>
2020-06-26 15:18:53 +0530

    Add files via upload

---
+++ Q3.py
@@ -0,0 +1,45 @@
+class Node:
+   
+    def __init__(self, key): 
+        self.data = key  
+        self.left = None
+        self.right = None
+  
+  
+def printLevelOrder(root): 
+    h = height(root) 
+    for i in range(1, h+1): 
+        printGivenLevel(root, i) 
+  
+   
+def printGivenLevel(root , level): 
+    if root is None: 
+        return
+    if level == 1: 
+        print(root.data,end=" ") 
+    elif level > 1 : 
+        printGivenLevel(root.left , level-1) 
+        printGivenLevel(root.right , level-1) 
+  
+  
+def height(node): 
+    if node is None: 
+        return 0 
+    else : 
+        lheight = height(node.left) 
+        rheight = height(node.right) 
+   
+        if lheight > rheight : 
+            return lheight+1
+        else: 
+            return rheight+1
+   
+root = Node(1) 
+root.right = Node(2) 
+root.right.right = Node(5) 
+root.right.right.left = Node(3) 
+root.right.right.right = Node(6) 
+root.right.right.left.right = Node(4) 
+  
+print("Level order traversal of binary tree is -") 
+printLevelOrder(root)
